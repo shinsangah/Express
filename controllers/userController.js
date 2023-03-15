8608a1ba4730d71564e0bd3088998c4b64d91c64
sang ah <kr.sangah@gmail.com>
2023-03-15 14:59:03 +0900

    230315

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,5 +4,16 @@
 const connection = require('./dbConnect');
 
 const userDB = {
-  getUsers: (cb) => {},
+  getUsers: (cb) => {
+    connection.query('SELECT * FROM mydb1.user;', (err, data) => {
+      if (err) throw err;
+      console.log(data);
+      cb(data);
+      // 콜백 함수에 데이터로 만드는 구조
+      // Mysql도 서버다. 여기에 보내는 행위는 통신. 당연히 동기적으로 처리해야 함.
+      // 나중에 async await 바꾸는 작업도 할 것임. 콜백에 데이터를 담아서 전달을 해주어야 함.
+    });
+  },
 };
+
+module.exports = userDB;
